e4edea3a52a29843926165344eb4918a36466546
utkarshnath <nath.utkarsh1@gmail.com>
2019-07-21 14:33:49 +0530

    added lecture 20

--- Lecture-20/Btree.cpp
+++ Lecture-20/Btree.cpp
@@ -50,11 +50,136 @@ void preorder(node * root){
     preorder(root->right);
     return;
 }
+void postorder(node * root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+    return;
+}
+void inorder(node * root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    return;
+}
+void levelOrderPrint(node * root){
+    queue<node *>q;
+    q.push(root);
+    while(!q.empty()){
+        node * top = q.front();
+        cout<<top->data<<" ";
+        q.pop();
+        if(top->left){
+            q.push(top->left);
+        }
+        if(top->right){
+            q.push(top->right);
+        }
+    }
+}
+int count(node * root){
+    if(root==NULL){
+        return 0;
+    }
+    int left = count(root->left);
+    int right = count(root->right);
+    return left+right+1;
+}
+int maxNode(node * root){
+    if(root==NULL){
+        return INT_MIN;
+    }
+    int left = maxNode(root->left);
+    int right = maxNode(root->right);
+    return max(root->data,max(left,right));
+}
+int greaterThanX(node * root,int x){
+    if(root==NULL){
+        return 0;
+    }
+    int left = greaterThanX(root->left,x);
+    int right = greaterThanX(root->right,x);
+    if(root->data > x){
+        return left+right+1;
+    }
+    return left+right;
+}
+int groupSum(node *root){
+    if(root==NULL){
+        return 0;
+    }
+    int sum = root->data;
+    if(root->left){
+        sum+=root->left->data;
+    }
+    if(root->right){
+        sum+=root->right->data;
+    }
+    return sum;
+}
+node * maxSubGroup(node* root){
+    if(root==NULL){
+        return NULL;
+    }
+    node * left = maxSubGroup(root->left);
+    node * right = maxSubGroup(root->right);
+    int leftGroup = groupSum(left);
+    int rightGroup = groupSum(right);
+    int rootGroup = groupSum(root);
+    node * max;
+    int maxGroup;
+    if(leftGroup>rightGroup){
+        max = left;
+        maxGroup = leftGroup;
+    }else{
+        max = right;
+        maxGroup = rightGroup;
+    }
+    if(rootGroup > maxGroup){
+        return root;
+    }else{
+        return max;
+    }
+}
+pair<node *,int> max1(pair<node *,int>a, pair<node *,int>b){
+    if(a.second>b.second){
+        return a;
+    }
+    return b;
+}
+pair<node *,int> maxSubGroup1(node * root){
+    if(root==NULL){
+        pair<node *,int> p(0,0);
+        return p;
+    }
+    pair<node *,int> left = maxSubGroup1(root->left);
+    pair<node *,int> right = maxSubGroup1(root->right);
+    pair<node *,int> rootpair;
+    rootpair.first = root;
+    rootpair.second = groupSum(root);
+    return max1(rootpair,max1(left,right));
+}
 //8 10 3 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
 int main(){
 node * root  = createBtree();
+/*
 preorder(root);
 cout<<endl;
+postorder(root);
+cout<<endl;
+inorder(root);
+cout<<endl;
+levelOrderPrint(root);
+cout<<endl;
+*/
+cout<<maxNode(root)<<endl;
+cout<<maxSubGroup1(root).second<<endl;
 }
 
 
