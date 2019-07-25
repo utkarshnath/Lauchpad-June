049e6485bb1d96908be41454eb52d540da8180f7
utkarshnath <nath.utkarsh1@gmail.com>
2019-07-25 11:45:36 +0530

    added lecture 23

--- Lecture-22/BST.cpp
+++ Lecture-22/BST.cpp
@@ -124,6 +124,83 @@ node * deleteElement(node * root,int data){
     }
     return root;
 }
+pair<node*,node*> llFromBst(node* root){
+    if(!root){
+        pair<node*,node*> p(0,0);
+        return p;
+    }
+    pair<node*,node*> left = llFromBst(root->left);
+    pair<node*,node*> right = llFromBst(root->right);
+    if(left.first==NULL){
+        left.first = root;
+        left.second = root;
+    }else{
+        left.second->left = root;
+    }
+    if(right.first==NULL){
+        root->left = NULL;
+        pair<node*,node*> p(left.first,root);
+        return p;
+    }else{
+        root->left = right.first;
+        right.second->left = NULL;
+        pair<node*,node*> p(left.first,right.second);
+        return p;
+    }
+}
+void printll(node * root){
+    while(root){
+        cout<<root->data<<"-->";
+        root = root->left;
+    }
+    cout<<"NULL"<<endl;
+}
+pair<bool,pair<int,int> > isBst(node * root){
+    if(!root){
+        return make_pair(1,make_pair(INT_MAX,INT_MIN));
+    }
+    pair<bool,pair<int,int> > left = isBst(root->left);
+    pair<bool,pair<int,int> > right = isBst(root->right);
+    if(left.first && right.first){
+        if(root->data > left.second.second && root->data<right.second.first){
+            int min = root->data;
+            if(root->left){
+                min = left.second.first;
+            }
+            int max = root->data;
+            if(root->right){
+                max = right.second.second;
+            }
+            return make_pair(1,make_pair(min,max));
+        }
+    }
+    return make_pair(0,make_pair(0,0));
+}
+bool checkBst(node * root){
+    static int prev = INT_MIN;
+    if(!root){
+        return true;
+    }
+    bool left = checkBst(root->left);
+    if(root->data < prev){
+        return false;
+    }
+    prev = root->data;
+    bool right = checkBst(root->right);
+    return left && right;
+}
+pair<bool,int> isbalanced(node * root){
+    if(!root){
+        return make_pair(1,0);
+    }
+    pair<bool,int> left = isbalanced(root->left);
+    pair<bool,int> right = isbalanced(root->right);
+    int lh = left.second;
+    int rh = right.second;
+    bool curr = left.first && right.first && abs(lh-rh)<=1;
+    int height = max(lh,rh)+1;
+    return make_pair(curr,height);
+}
 int main(){
 node * root = NULL;
 addElement(root,8);addElement(root,3);addElement(root,10);
@@ -132,8 +209,12 @@ addElement(root,13);addElement(root,4);addElement(root,7);
 addElement(root,2);
 //root = deleteElement(root,4);
 //deleteElement(root,10);
-root = deleteElement(root,6);
-printLevelWisePrint(root);
+//root = deleteElement(root,6);
+//printLevelWisePrint(root);
+//pair<node*,node*> p = llFromBst(root);
+//printll(p.first);
+pair<bool,pair<int,int> > p =  isBst(root);
+cout<<p.first<<" "<<p.second.first<<" "<<p.second.second<<endl;
 }
 
 
